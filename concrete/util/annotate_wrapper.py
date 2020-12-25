0ea9d2270baa2bf440a0a4efd87132a10eedb8a1
Chandler May <chandler.camille.may@gmail.com>
2020-12-25 13:36:04 -0500

    Add batch annotation service.

--- concrete/util/annotate_wrapper.py
+++ concrete/util/annotate_wrapper.py
@@ -1,10 +1,41 @@
-from ..annotate import AnnotateCommunicationService
+from ..annotate import (
+    AnnotateCommunicationBatchService,
+    AnnotateCommunicationService,
+    AnnotateWithContextService)
 from .service_wrapper import (
     ConcreteServiceClientWrapper,
     ConcreteServiceWrapper,
     HTTPConcreteServiceClientWrapper,
     SubprocessConcreteServiceWrapper)
 
+
+# AnnotateCommunicationBatchService wrappers
+
+AnnotateCommunicationBatchClientWrapper = type(
+    'AnnotateCommunicationBatchClientWrapper',
+    (ConcreteServiceClientWrapper,),
+    {'concrete_service_class': AnnotateCommunicationBatchService})
+
+
+AnnotateCommunicationBatchServiceWrapper = type(
+    'AnnotateCommunicationBatchServiceWrapper',
+    (ConcreteServiceWrapper,),
+    {'concrete_service_class': AnnotateCommunicationBatchService})
+
+
+HTTPAnnotateCommunicationBatchClientWrapper = type(
+    'HTTPAnnotateCommunicationBatchClientWrapper',
+    (HTTPConcreteServiceClientWrapper,),
+    {'concrete_service_class': AnnotateCommunicationBatchService})
+
+
+SubprocessAnnotateCommunicationBatchServiceWrapper = type(
+    'SubprocessAnnotateCommunicationBatchServiceWrapper',
+    (SubprocessConcreteServiceWrapper,),
+    {'concrete_service_wrapper_class': AnnotateCommunicationBatchServiceWrapper})
+
+
+# AnnotateCommunicationService wrappers
 
 AnnotateCommunicationClientWrapper = type(
     'AnnotateCommunicationClientWrapper',
@@ -28,3 +59,29 @@ SubprocessAnnotateCommunicationServiceWrapper = type(
     'SubprocessAnnotateCommunicationServiceWrapper',
     (SubprocessConcreteServiceWrapper,),
     {'concrete_service_wrapper_class': AnnotateCommunicationServiceWrapper})
+
+
+# AnnotateWithContextService wrappers
+
+AnnotateWithContextClientWrapper = type(
+    'AnnotateWithContextClientWrapper',
+    (ConcreteServiceClientWrapper,),
+    {'concrete_service_class': AnnotateWithContextService})
+
+
+AnnotateWithContextServiceWrapper = type(
+    'AnnotateWithContextServiceWrapper',
+    (ConcreteServiceWrapper,),
+    {'concrete_service_class': AnnotateWithContextService})
+
+
+HTTPAnnotateWithContextClientWrapper = type(
+    'HTTPAnnotateWithContextClientWrapper',
+    (HTTPConcreteServiceClientWrapper,),
+    {'concrete_service_class': AnnotateWithContextService})
+
+
+SubprocessAnnotateWithContextServiceWrapper = type(
+    'SubprocessAnnotateWithContextServiceWrapper',
+    (SubprocessConcreteServiceWrapper,),
+    {'concrete_service_wrapper_class': AnnotateWithContextServiceWrapper})
