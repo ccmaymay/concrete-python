39dd300d84ebdb3bb0d6b30673a79813a631d2fe
Chandler May <457238+ccmaymay@users.noreply.github.com>
2020-12-25 19:29:17 -0500

    Export AnnotateCommunicationBatchService.

--- concrete/annotate/__init__.py
+++ concrete/annotate/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['ttypes', 'constants', 'AnnotateCommunicationService', 'AnnotateWithContextService']
+__all__ = ['ttypes', 'constants', 'AnnotateCommunicationBatchService', 'AnnotateCommunicationService', 'AnnotateWithContextService']
